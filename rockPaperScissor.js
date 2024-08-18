ba51cad670c1dd24ad81c4d2e2ddaf7fba72f1cd
psyeh7 <psyeh7@exmail.nottingham.ac.uk>
2024-08-18 16:44:57 +0100

    Began rock paper scissors JS

---
+++ rockPaperScissor.js
@@ -0,0 +1,13 @@
+console.log('hi');
+
+const getUserChoice = (userInput)=>{
+  userInput = userInput.toLowerCase();
+
+  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+    return userInput;
+  }
+  else{
+    console.log("Error!")
+  }
+}
+
